5c9b8ab00a291ac5b4c49c2d4ef8f2d8c577cecb
Nicoly Lopes Araujo <157145216+NicolyLopesAraujo@users.noreply.github.com>
2024-08-27 21:18:06 -0300

    Add files via upload

---
+++ Lista01/Medias.java
@@ -0,0 +1,12 @@
+
+public class Medias {
+	public static void main(String args[]) {
+		//declaração de variaveis
+		
+		double n1,n2, m;
+		n1 = 10;
+	    n2 = 1.5;
+	    m = (n1+n2)/2;
+	    System.out.println("Sua média é " + m+" pontos");
+	}
+}
