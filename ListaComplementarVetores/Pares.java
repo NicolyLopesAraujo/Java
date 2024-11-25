b43158304c3a90f397036ad6621b3d7c12a15397
Nicoly Lopes Araujo <157145216+NicolyLopesAraujo@users.noreply.github.com>
2024-11-25 10:04:28 -0300

    Update Pares.java

--- ListaComplementarVetores/Pares.java
+++ ListaComplementarVetores/Pares.java
@@ -7,16 +7,22 @@ public class Pares {
 		final int TAM = 10;
 		int i,j,a[];
 		a = new int [TAM];
-		
+
+		//Leitura dos valores
 		for(i=0;i<TAM;i++) {
 			System.out.println("Digite "+(i+1)+" números");
 			a[i] = ler.nextInt(); }
-		for(j=0;i<=a[i];j=j+2) {
+		//Apresentação 
+		for(i=0;i<10;i++) {
+			System.out.println("Elemento a[ " + i + "] = " + a[i]);
+			System.out.println("Os pares são: ");
+		}
+		//Esse serve para verificar os números pares, o j ele serve para ir contando ate quando o elemento do vetor for menor ou igual a ele mesmo 
+		for(j=0;i<=a[i];j++) {
+		// Aqui ira apresentar os numeros pares
 		    System.out.println(j);
-		    if(j<a[i]-1) {
+		    if(j%2==0) {
 				System.out.print(j+" - ");
-			}else {
-				System.out.print(j);
 			}
 		 }
 	  
