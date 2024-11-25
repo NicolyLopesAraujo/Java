29a2749efec727759f5d90c696176619035e1d45
Nicoly Lopes Araujo <157145216+NicolyLopesAraujo@users.noreply.github.com>
2024-11-25 08:17:03 -0300

    Update ParEImpar.java

--- ListaComplementarVetores/ParEImpar.java
+++ ListaComplementarVetores/ParEImpar.java
@@ -12,16 +12,18 @@ public class ParEImpar {
 		b = new int [TAM];
 
 		par=0;
+		//Isso serve para exibir os impares depois dos pares
 		impar=TAM-1;
 		
-		
+		//Leitura dos números
 		for (i=0; i<TAM; i++) {
 			System.out.println("Entre com "+(i+1)+" valores:");
 			a[i] = ler.nextInt();
-			
+		//Este serve para se o resto for zero,quando divididos por 2,ele vai adicionar mais 1 no contador do par e adicionar esse número no vetor b
 			if (a[i]%2==0) {
  				b[par] = a[i];
 				par++;
+		//Mas caso não ele ira adicionar no vetor b o impar, porem do fim ao começo, assim fazendo com que o impar apareça por ultimo 
 			}else {
 				b[impar] = a[i];
 				impar--;
@@ -29,6 +31,7 @@ public class ParEImpar {
 		
 		
 		}
+		//Já este serve para apresentar o números pelo vetor b
 		for (i=0;i<TAM;i++) {
 			System.out.println(b[i]);
 		
